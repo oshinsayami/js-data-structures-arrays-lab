b59a55706c508cea5c4cbae52c230f9530190832
Simran <mdrsimran@gmail.com>
2021-01-28 13:18:56 -0600

    commit tree 26a1fefd41d6b869192f84b715e62825feb935a6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendDriver(){
+    drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver(){
+    drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(){
+drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+ drivers.shift();
+}
+
+function appendDriver(name){
+    return [...drivers, name];
+}
+
+function prependDriver(name){
+    return [name, ...drivers];
+}
+
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
+
+function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1);
+}
